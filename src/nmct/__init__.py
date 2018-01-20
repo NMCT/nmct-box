6008897add825d5e2666334609ad5fc92ed336cb
Sebastian Holzer <sebastian.holzer@howest.be>
2018-01-20 08:45:16 +0100

    hopefully resolved circular dependency

--- src/nmct/__init__.py
+++ src/nmct/__init__.py
@@ -1,5 +1,5 @@
-from . import box, hardware, settings, snowboy, watson, web
-from .apis.snowboy import HotWord
 from .drivers.neopixel import Color
+from .apis.snowboy import HotWord
+from . import box, hardware, settings, snowboy, watson, web
 
 __ALL__ = ['box', 'watson', 'snowboy', 'hardware', 'Color', 'HotWord', 'web']
