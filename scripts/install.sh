026aa0d59274ed18e36713ba38d5adfb896b600b
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-01-20 02:18:19 +0100

    install & prepare_image scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,81 +13,106 @@ fi
 
 set -o errexit
 
-export NMCT_HOME="$(dirname "${PWD}")"
-readonly VENVDIR="${NMCT_HOME}/env"
-readonly TEMPDIR="/tmp/nmct-box"
-readonly APT_PKG="python3-dev python3-venv swig libatlas-base-dev scons libffi-dev portaudio19-dev \
-python3-pyaudio sox libssl-dev nginx ufw python3-notebook npm"
-readonly NPM_PKG="configurable-http-proxy"
-readonly PIP_PKG="setuptools wheel distutils"
+[[ -z ${NMCT_HOME} ]] && export NMCT_HOME="$(dirname "${PWD}")"
+echo "Installing to ${NMCT_HOME}"
+readonly PYENV="${NMCT_HOME}/env/bin/python"
 
-# install APT packages
-sudo apt update -y && apt install -y ${APT_PKG}
-
-# install NPM packages
-npm install -g ${NPM_PKG}
-
-# create venv
-python3 -m venv ${VENVDIR}
-${VENVDIR}/bin/python -m pip install --upgrade pip ${PIP_PKG}
-source ${VENVDIR}/bin/activate
-
-# install Python packages
-${VENVDIR}/bin/python -m pip install -r "${NMCT_HOME}/src/requirements.txt"
+# dependencies
+readonly APT_PACKAGES="python3-dev python3-venv swig libatlas-base-dev scons libffi-dev portaudio19-dev \
+            python3-pyaudio sox libssl-dev nginx python3-notebook npm"
+readonly NPM_PACKAGES="configurable-http-proxy"
+readonly PIP_PACKAGES="setuptools wheel distutils"
 
 
-# pull & install AIY drivers
-dir="${NMCT_HOME}/aiy-voicekit"
-git clone https://github.com/google/aiyprojects-raspbian.git "${dir}"
-pushd "${dir}"
-git checkout aiyprojects
-./scripts/install-deps.sh
-sudo ./scripts/install-services.sh
-sudo ./scripts/configure-driver.sh
-sudo ./scripts/install-alsa-config.sh
-pushd "${dir}/src"
-${VENVDIR}/bin/python setup.py install
-popd
+install_apt_packages(){
+    sudo apt update -y && sudo apt install -y ${APT_PACKAGES}
+}
 
-# pull & install Snowboy
-dir=${TEMPDIR}/snowboy
-git clone https://github.com/Kitt-AI/snowboy.git ${dir}
-pushd ${dir}
-${VENVDIR}/bin/python setup.py build install
-popd
+install_npm_packages(){
+    npm install -g ${NPM_PACKAGES}
+}
 
-# pull & install NeoPixel driver
-dir=${TEMPDIR}/rpi-ws821x
-git clone https://github.com/jgarff/rpi_ws281x.git ${dir}
-pushd ${dir}
-scons
-pushd ${dir}/python
-${VENVDIR}/bin/python setup.py build install
+create_venv(){
+    python3 -m venv "${NMCT_HOME}/env"
+    ${PYENV} -m pip install --upgrade pip ${PIP_PACKAGES}
+}
 
-# install our package(s)
-pushd "${NMCT_HOME}/src"
-python setup.py install
+install_aiy_voicekit(){
+    # pull & install AIY drivers
+    dir="${NMCT_HOME}/aiy-voicekit"
+    git clone https://github.com/google/aiyprojects-raspbian.git "${dir}"
+    pushd "${dir}"
+    git checkout aiyprojects
+    ./scripts/install-deps.sh
+    sudo ./scripts/install-services.sh
+    sudo ./scripts/configure-driver.sh
+    sudo ./scripts/install-alsa-config.sh
+    popd
+    pushd "${dir}/src"
+    ${PYENV} setup.py install
+    popd
+}
 
-deactivate
+install_snowboy(){
+    # pull & install Snowboy
+    dir=${NMCT_HOME}/snowboy
+    git clone https://github.com/Kitt-AI/snowboy.git ${dir}
+    pushd ${dir}
+    ${PYENV} setup.py build install
+    popd
+}
 
-# install services
-sudo -s
-echo "export NMCT_HOME=${NMCT_HOME}" > /etc/profile.d/nmct_box.sh
+install_neopixel(){
+    # pull & install NeoPixel driver
+    dir=${NMCT_HOME}/rpi-ws821x
+    git clone https://github.com/jgarff/rpi_ws281x.git ${dir}
+    sed -i 's/^\(\#define GPIO_PIN\W*\)18$/\112/g' "${dir}/main.c"
+    pushd ${dir} && scons && popd
+    pushd ${dir}/python
+    ${PYENV} setup.py build install
+    popd
+}
 
-for file in ${NMCT_HOME}/systemd/*; do
-    cat ${file} | envsubst > "/etc/systemd/system/$(basename ${file})"
-    systemctl daemon-reload
-    systemctl enable "$(basename ${file})"
-    systemctl start "$(basename ${file})"
-done
+install_nmct_box(){
+    # install our package(s)
+    pushd "${NMCT_HOME}/src"
+    ${PYENV} -m pip install -r requirements.txt
+    ${PYENV} setup.py install
+    popd
+}
 
-if [[ -f /etc/nginx/sites-enabled/default ]]; then
-    rm /etc/nginx/sites-enabled/default
-fi
-cat "${NMCT_HOME}/resources/conf/nginx" | envsubst '${NMCT_HOME}' > /etc/nginx/sites-available/nmct-box
-ln -s /etc/nginx/sites-available/nmct-box /etc/nginx/sites-enabled/nmct-box
-systemctl restart nginx
+#deactivate
+#sudo -s
+
+install_services(){
+    for file in ${NMCT_HOME}/systemd/*; do
+        cat ${file} | envsubst | sudo tee "/etc/systemd/system/$(basename ${file})"
+    #    systemctl daemon-reload
+        sudo systemctl enable "$(basename ${file})"
+    #    systemctl start "$(basename ${file})"
+    done
+
+    if [[ -f /etc/nginx/sites-enabled/default ]]; then
+        sudo rm /etc/nginx/sites-enabled/default
+    fi
+
+    cat "${NMCT_HOME}/resources/conf/nginx" | envsubst '${NMCT_HOME}' | sudo tee /etc/nginx/sites-available/nmct-box
+
+    if [[ ! -f /etc/nginx/sites-enabled/nmct-box ]]; then
+        sudo ln -s /etc/nginx/sites-available/nmct-box /etc/nginx/sites-enabled/nmct-box
+    fi
+    #systemctl restart nginx
+}
 
 
 
+install_apt_packages
+install_npm_packages
+create_venv
+install_aiy_voicekit
+install_snowboy
+install_neopixel
+install_nmct_box
+install_services
 
+printf "Done. If you just installed the AIY drivers for the first time, reboot and run '%s/aiy-voicekit/checkpoints/check_audio.py'\n" ${NMCT_HOME}
---
+++ scripts/prepare_image.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+     printf "This script should be run as root, try 'sudo %s'\n" ${0} >&2
+     exit 1
+fi
+
+set -o errexit
+
+export NMCT_HOME="/home/nmct/nmct-box"
+readonly new_user=nmct
+readonly new_pass=smartthings
+
+
+readonly APT_PACKAGES="etckeeper vim lsof telnet dnsutils at screen git ufw"
+
+update_raspbian(){
+    apt update -y &&
+    apt install -y ${APT_PACKAGES}
+    apt full-upgrade -y --autoremove
+}
+
+create_user(){
+    echo "Creating user ${new_user}"
+    cp -R /home/pi/* /etc/skel # && chown -R root:root /etc/skel/*
+    groups=$(id pi -Gn | sed 's/^pi //g' | sed 's/ /,/g')
+    useradd ${new_user} -s /bin/bash -m -G ${groups}
+    echo "${new_user}:${new_pass}" | chpasswd
+    tee "/etc/sudoers.d/011_${new_user}-nopasswd" < sed "s/^pi/${new_user}/" /etc/sudoers.d/010_pi-nopasswd
+    chage -E 0 pi
+}
+
+do_system_settings(){
+    # system config
+    echo "Updating system settings..."
+    new_hostname="nmct-box-$(cut -d: -f4- < /sys/class/net/wlan0/address | tr -d :)"
+    cmd='raspi-config nonint'
+    ${cmd} do_hostname ${new_hostname}
+    ${cmd} do_wifi_country 'BE'
+    ${cmd} do_change_timezone 'Europe/Brussels'
+    ${cmd} do_configure_keyboard 'be'
+    su - ${cmd} do_boot_behaviour B1 #B1=console, B3=Desktop, n+1=autologon
+    ${cmd} do_boot_wait 1
+    ${cmd} do_boot_splash 1
+
+    # interfaces
+    ${cmd} do_ssh 0
+    ${cmd} do_spi 0
+    ${cmd} do_i2c 0
+    ${cmd} do_onewire 0
+
+    echo "export NMCT_HOME=${NMCT_HOME}" > /etc/profile.d/nmct_box.sh
+    echo "alias ll='ls -al'" >> /etc/profile.d/nmct_box.sh
+}
+
+
+do_finish(){
+    echo -e "\n\Done! User 'pi' will be disabled, after rebooting you can connect with: \n
+        hostname: \t\t\033[32m${new_hostname}\033[0m
+        user: \t\t\033[32m${new_user}\033[0m
+        password:\t\t\033[32m${new_pass}\033[0m
+        \nPress any key to reboot...\n"
+    read -sn 1
+    rm -f "${BASH_SOURCE}"
+    reboot
+}
+
+
+#########################################################
+
+update_raspbian
+do_system_settings
+create_user
+
+su - nmct
+git clone https://github.com/nmctseb/nmct-box.git "${NMCT_HOME}"
+source "${NMCT_HOME}/scripts/install.sh"
+do_finish
