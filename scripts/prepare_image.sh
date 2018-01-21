2f5bb1b6414b80e4908b91c1f2db8a57bc7c6d33
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-01-21 17:56:57 +0100

    scripts, services

--- scripts/prepare_image.sh
+++ scripts/prepare_image.sh
@@ -11,16 +11,16 @@ source ./nmct-box.sh
 
 do_system_settings
 new_hostname=$(change_hostname ${HOSTNAME_PREFIX})
-update_raspbian
 new_default_user ${CREATE_USER} ${PASSWORD}
+update_raspbian
 
 
 
-echo -e "\n\nDone! User 'pi' will be disabled, after rebooting you can connect with: \n
+echo -e "\n\n\n\nDone! User 'pi' will be disabled, after rebooting you can connect with: \n
     hostname:\t\033[32m${new_hostname}\033[0m
     username:\t\033[32m${CREATE_USER}\033[0m
     password:\t\033[32m${PASSWORD}\033[0m
-    \nPress any key to reboot...\n"
+    \nPress any key to reboot...\n\n\n"
 read -sn 1
 chage -E 0 pi
 usermod -L pi
