b7c0483ac76219792f432ab2b9bacb8d8aa63153
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-01-23 00:28:54 +0100

    file upload on dashboard

--- src/nmct/web/dashboard.py
+++ src/nmct/web/dashboard.py
@@ -2,9 +2,20 @@ from random import randint
 from traceback import print_exception
 
 import flask
+from flask import request
+
 import nmct
+import os
+from flask import request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'htm', 'html', 'png', 'jpg', 'jpeg', 'gif', 'py'}
 
 app = flask.Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = '/home/nmct/uploads'
+
 display = nmct.box.get_display()
 
 
@@ -80,6 +91,32 @@ def show_student():
 
     print(template)
     return flask.render_template(template)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    w1ids = nmct.box.list_onewire_ids()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            show_text="Upload successful"
+    return flask.render_template("dashboard.html", show_method='show_temperature', show_text=show_text, w1ids=w1ids)
 
 
 if __name__ == '__main__':
--- src/nmct/web/student.py
+++ src/nmct/web/student.py
@@ -1,39 +1,7 @@
 import flask
-import os
-from flask import request, flash, redirect, url_for
-from werkzeug.utils import secure_filename
-
-ALLOWED_EXTENSIONS = {'htm', 'html', 'png', 'jpg', 'jpeg', 'gif', 'py'}
 
 app = flask.Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-app.config['UPLOAD_FOLDER'] = '/home/nmct/uploads'
 
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return flask.render_template("student.html")
 
 
 if __name__ == '__main__':
