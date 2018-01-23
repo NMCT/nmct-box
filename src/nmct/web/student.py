7522ad9c2826d9265672d306ccbefe757268b4bd
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-01-23 08:14:50 +0100

    web ui

--- src/nmct/web/student.py
+++ src/nmct/web/student.py
@@ -2,7 +2,20 @@ from pathlib import Path
 
 from .app import *
 
+app = flask.Flask(__name__)
+
+
+@app.route('/student', methods=['POST', 'GET'])
+def show_student():
+    # http://169.254.10.11/student?number=3
+    number = flask.request.args.get('number')
+    if number is None:
+        error = 'Gelieve een studentnummer mee te geven : http://xxx.xxx.xxx.xxx/student?number=x'
+        return flask.render_template("error.html", exc=None, message=error)
+    template = "student{0}.html".format(number)
+    # print(template)
+    return flask.render_template(template)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3006, debug=True)
+    app.run(debug=True)
