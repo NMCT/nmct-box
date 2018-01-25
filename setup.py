78858b0e88e249536653bcd6c7afac383f62636f
nmctseb <29257008+nmctseb@users.noreply.github.com>
2018-01-25 11:04:20 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NMCT-Box',
-    version='0.4.5',
+    version='1.0.0',
     description='NMCT Box framework',
     package_dir={'': 'src'},
     packages=['nmct', 'nmct.drivers', 'nmct.apis', 'nmct.web'],
